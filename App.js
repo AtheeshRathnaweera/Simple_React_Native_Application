12ca62ea68e44a55f01c273c83f66201a518ea73
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-04-19 01:41:37 +0530

    Hello world test

--- App.js
+++ App.js
@@ -20,11 +20,9 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+             <Text>Hello, world!</Text>
+           </View>
     );
   }
 }
